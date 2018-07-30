a0a92dfcd25f4fc0e766fc4a126edf81d939d99d
Adrian Pop <36763899+popadrianc@users.noreply.github.com>
2018-07-30 15:00:04 +0300

    small counting program
    add small counting program

---
+++ Exercises/ex3.py
@@ -0,0 +1,24 @@
+#This should be a counting program
+print("I will start counting cakes")
+
+print("Banana cake", 25 + 30 / 6)
+print("Apple cake", 100 - 25 * 3 % 4)
+
+print("Now I will count the syrup:")
+
+print( 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 +2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?, 5 > -2")
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
