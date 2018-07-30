9764edad50a1ad0c16c4312adaf0ca27f9b500de
Adrian Pop <36763899+popadrianc@users.noreply.github.com>
2018-07-30 14:36:04 +0300

    2 exercises
    2 exercises

---
+++ Exercises/ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read what does this program
+# Anything after the # is ignored by python
+
+print ("Here should be a sample code") # sample code
+
+# Using this you can disable or comment out code, like the following :
+# print ("this was a code that now is disabled")
+
+print ("This code is a will work as it isn't commented")
