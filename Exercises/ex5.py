6b18adbe0a5d025741baaaca82ce0be220060428
Adrian Pop <36763899+popadrianc@users.noreply.github.com>
2018-08-01 14:41:16 +0300

    Variables and Printing 2
    More Variables and Printing

---
+++ Exercises/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Adrian Pop'
+my_age = 35 # pe bune
+my_height = 184 # cm
+my_weight = 121 # kg
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_heigh + my_weight
+print(f"If I add{my_age}, {my_height}, and {my_weight} I get {total} ")
